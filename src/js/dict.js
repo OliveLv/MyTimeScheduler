d2df4dd43cee607a49179d46bd6fb6e0515ddb9b
olivelv <adalxh@gmail.com>
2016-06-09 19:09:05 +0800

    source code

---
+++ src/js/dict.js
@@ -0,0 +1,24 @@
+module.exports = {
+    event_label: {
+        "投递截至": "submit-end",
+		"投递": "submit",
+		"内推": "recommend",
+		"笔试": "examination",
+		"一面": "interview-one",
+		"二面": "interview-two",
+		"三面": "interview-three",
+		"offer": "offer",
+		"终止": "end",
+    },
+	item_status:{
+		"未投递": "none",
+		"投递": "underway",
+		"内推": "underway",
+		"笔试": "underway",
+		"一面": "underway",
+		"二面": "underway",
+		"三面": "underway",
+		"offer": "success",
+		"终止": "fail",
+	}
+};
